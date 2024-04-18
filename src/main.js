95139db0160ce12dee0b40012ffaf7536973dcfd
KrispWaffle <ijwu345@gmail.com>
2024-04-17 22:56:27 -0700

    fixed lots of stuff
    Alright i basically made the password manager, i still need to add some little things here and there and definitely need to improve on how it looks but overall decent progress. also need to add saving it so when a user comes back they have their stuff saved

--- src/main.js
+++ src/main.js
@@ -1,13 +1,71 @@
-var enterInfo = document.getElementById('enterInfo')
-var submit = document.getElementById('submit')
-const webInput = document.getElementById("webInput");
-const userInput = document.getElementById("userInput");
-const passInput = document.getElementById("passInput");
+let decryptedData;
 
-var password = "jgon"
-var encrypted = CryptoJS.AES.encrypt(password, "secretkey123").toString();
-console.log("Encrypted:", encrypted);
+var enterInfo = document.getElementById('enterInfo');
+var addInfo = document.getElementById('addPass');
+enterInfo.style.display = 'none';
+function createinfo(a){
+    const container = document.getElementById('middle'); // Get the container element
+    const newDiv = document.createElement('div')
+    newDiv.innerText = a; 
+    newDiv.style.color = "White"; 
+    newDiv.style.backgroundColor = 'grey'
+    newDiv.style.borderRadius = '10px'
+    newDiv.style.margin = '20px'
+    newDiv.style.textAlign = 'center'
+    newDiv.style.alignSelf = 'center'
+    newDiv.style.width = '200px'
+    newDiv.style.height = '150px'
+    container.appendChild(newDiv); 
+    console.log('created div and appended to container');
+}
 
-// Decryption
-var decrypted = CryptoJS.AES.decrypt(encrypted, "secretkey123").toString(CryptoJS.enc.Utf8);
-console.log("Decrypted:", decrypted);
+function theproc(username, password, website) {
+   /* const website = document.getElementById("webInput").value;
+    const username = document.getElementById("userInput").value;
+    const password = document.getElementById("passInput").value;
+    */
+
+    
+
+    var encryptedp = CryptoJS.AES.encrypt(password, "secretkey123").toString();
+    console.log("Encrypted Password:", encryptedp);
+    var decryptedp = CryptoJS.AES.decrypt(encryptedp, "secretkey123").toString(CryptoJS.enc.Utf8);
+    console.log("Decrypted Password:", decryptedp);
+
+    var encryptedu = CryptoJS.AES.encrypt(username, "secretkey123").toString();
+    console.log("Encrypted Username:", encryptedu);
+    var decryptedu = CryptoJS.AES.decrypt(encryptedu, "secretkey123").toString(CryptoJS.enc.Utf8);
+    console.log("Decrypted Username:", decryptedu);
+
+    var encryptedw = CryptoJS.AES.encrypt(website, "secretkey123").toString();
+    console.log("Encrypted Website:", encryptedw);
+    var decryptedw = CryptoJS.AES.decrypt(encryptedw, "secretkey123").toString(CryptoJS.enc.Utf8);
+    console.log("Decrypted Website:", decryptedw);
+    return "Username: " + username +" password: "+ password + 
+    "Wesbite: "+ website;
+    
+}
+
+addInfo.addEventListener('click', handler);
+
+function handler() {
+    addInfo.style.display = 'none';
+    enterInfo.style.display = 'grid';
+    console.log('show');
+}
+
+document.getElementById("submit").addEventListener("click", () => {
+    const website = document.getElementById("webInput").value;
+    const username = document.getElementById("userInput").value;
+    const password = document.getElementById("passInput").value;
+    theproc(username, password, website);
+    console.log(theproc(username, password, website))
+    createinfo(theproc(username, password, website));
+    addInfo.style.display = 'flex';
+    enterInfo.style.display = 'none';
+});
+
+document.getElementById('cancel').addEventListener('click', () => {
+    addInfo.style.display = 'flex';
+    enterInfo.style.display = 'none';
+});
--- src/test.js
+++ src/test.js
@@ -1,34 +1,35 @@
-let decryptedData;
-
 var enterInfo = document.getElementById('enterInfo');
 var addInfo = document.getElementById('addPass');
 enterInfo.style.display = 'none';
 
-function theproc() {
-    const website = document.getElementById("webInput").value;
-    const username = document.getElementById("userInput").value;
-    const password = document.getElementById("passInput").value;
-
-    console.log(username);
-    console.log(password);
-    console.log(website);
-
-    var encryptedp = CryptoJS.AES.encrypt(password, "secretkey123").toString();
-    console.log("Encrypted Password:", encryptedp);
-    var decryptedp = CryptoJS.AES.decrypt(encryptedp, "secretkey123").toString(CryptoJS.enc.Utf8);
-    console.log("Decrypted Password:", decryptedp);
-
-    var encryptedu = CryptoJS.AES.encrypt(username, "secretkey123").toString();
-    console.log("Encrypted Username:", encryptedu);
-    var decryptedu = CryptoJS.AES.decrypt(encryptedu, "secretkey123").toString(CryptoJS.enc.Utf8);
-    console.log("Decrypted Username:", decryptedu);
-
-    var encryptedw = CryptoJS.AES.encrypt(website, "secretkey123").toString();
-    console.log("Encrypted Website:", encryptedw);
-    var decryptedw = CryptoJS.AES.decrypt(encryptedw, "secretkey123").toString(CryptoJS.enc.Utf8);
-    console.log("Decrypted Website:", decryptedw);
+function createinfo(username, password, website) {
+    const container = document.getElementById('middle');
+    const newDiv = document.createElement('div');
+    const text  = document.createElement('p')
+    
+    newDiv.style.color = "white";
+    newDiv.style.backgroundColor = 'grey';
+    newDiv.style.borderRadius = '10px';
+    newDiv.style.margin = '20px';
+    newDiv.style.display = 'grid'
+    newDiv.style.width = 'autopx';
+    newDiv.style.height = '150px';
+    container.appendChild(newDiv);
+    newDiv.appendChild(text)
+    text.style.textAlign = 'center'
+    text.style.padding = '29px'
+    text.innerText = `Website: ${website}\nUsername: ${username}\nPassword: ${password}`; 
+    
 }
 
+
+function encrypt(data) {
+    return CryptoJS.AES.encrypt(data, "secretkey123").toString();
+}
+
+function decrypt(encryptedData) {
+    return CryptoJS.AES.decrypt(encryptedData, "secretkey123").toString(CryptoJS.enc.Utf8);
+}
 addInfo.addEventListener('click', handler);
 
 function handler() {
@@ -36,9 +37,22 @@ function handler() {
     enterInfo.style.display = 'grid';
     console.log('show');
 }
-
 document.getElementById("submit").addEventListener("click", () => {
-    theproc();
+    const website = document.getElementById("webInput").value;
+    const username = document.getElementById("userInput").value;
+    const password = document.getElementById("passInput").value;
+
+    const encryptedUsername = encrypt(username);
+    const encryptedPassword = encrypt(password);
+    const encryptedWebsite = encrypt(website);
+
+    createinfo(username, password, website);
+    addInfo.style.display = 'flex';
+    enterInfo.style.display = 'none';
+
+    console.log("Encrypted Username:", encryptedUsername);
+    console.log("Encrypted Password:", encryptedPassword);
+    console.log("Encrypted Website:", encryptedWebsite);
 });
 
 document.getElementById('cancel').addEventListener('click', () => {
