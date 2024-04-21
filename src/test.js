9745f8eef4d0a7ac5f69d79d436a48aabee3de64
KrispWaffle <ijwu345@gmail.com>
2024-04-21 15:10:14 -0700

    changed lots of thigns
    changed  so i hopefully stores the past data the user has inputed

--- src/test.js
+++ src/test.js
@@ -1,27 +1,31 @@
 var enterInfo = document.getElementById('enterInfo');
 var addInfo = document.getElementById('addPass');
-enterInfo.style.display = 'none';
 
-function createinfo(username, password, website) {
-    const container = document.getElementById('middle');
+var middle = document.getElementById('middle');
+middle.style.height = window.innerHeight + 'px';
+middle.style.overflowY = 'auto'; 
+var counter = 1;
+var userS = JSON.parse(localStorage.getItem('userS')) || [];
+enterInfo.style.display = 'none'; 
+
+function createinfo(user) {
     const newDiv = document.createElement('div');
-    const text  = document.createElement('p')
-    
+    const text = document.createElement('p');
+
     newDiv.style.color = "white";
     newDiv.style.backgroundColor = 'grey';
     newDiv.style.borderRadius = '10px';
     newDiv.style.margin = '20px';
-    newDiv.style.display = 'grid'
     newDiv.style.width = 'autopx';
     newDiv.style.height = '150px';
-    container.appendChild(newDiv);
-    newDiv.appendChild(text)
-    text.style.textAlign = 'center'
-    text.style.padding = '29px'
-    text.innerText = `Website: ${website}\nUsername: ${username}\nPassword: ${password}`; 
-    
-}
+    middle.appendChild(newDiv); 
 
+    newDiv.appendChild(text);
+    text.style.textAlign = 'center';
+    text.style.padding = '29px';
+    text.innerText = `Website: ${user.web}\nUsername: ${user.name}\nPassword: ${user.pass}`;
+
+}
 
 function encrypt(data) {
     return CryptoJS.AES.encrypt(data, "secretkey123").toString();
@@ -30,6 +34,7 @@ function encrypt(data) {
 function decrypt(encryptedData) {
     return CryptoJS.AES.decrypt(encryptedData, "secretkey123").toString(CryptoJS.enc.Utf8);
 }
+
 addInfo.addEventListener('click', handler);
 
 function handler() {
@@ -38,24 +43,33 @@ function handler() {
     console.log('show');
 }
 document.getElementById("submit").addEventListener("click", () => {
-    const website = document.getElementById("webInput").value;
-    const username = document.getElementById("userInput").value;
-    const password = document.getElementById("passInput").value;
+    var website = document.getElementById("webInput").value;
+    var username = document.getElementById("userInput").value;
+    var password = document.getElementById("passInput").value;
 
-    const encryptedUsername = encrypt(username);
-    const encryptedPassword = encrypt(password);
-    const encryptedWebsite = encrypt(website);
+    let encryptedUsername = encrypt(username);
+    let encryptedPassword = encrypt(password);
+    let encryptedWebsite = encrypt(website);
 
-    createinfo(username, password, website);
+    userS.push({ id: counter++, name: encryptedUsername, pass: encryptedPassword, web: encryptedWebsite });
+    localStorage.setItem('userS', JSON.stringify(userS));
+
+    createinfo({ web: website, name: username, pass: password });
+
     addInfo.style.display = 'flex';
     enterInfo.style.display = 'none';
 
-    console.log("Encrypted Username:", encryptedUsername);
-    console.log("Encrypted Password:", encryptedPassword);
-    console.log("Encrypted Website:", encryptedWebsite);
+   
+    document.getElementById("webInput").value = "";
+    document.getElementById("userInput").value = "";
+    document.getElementById("passInput").value = "";
 });
 
 document.getElementById('cancel').addEventListener('click', () => {
     addInfo.style.display = 'flex';
     enterInfo.style.display = 'none';
+});
+
+userS.forEach((user) => {
+    createinfo({ web: decrypt(user.web), name: decrypt(user.name), pass: decrypt(user.pass) });
 });
